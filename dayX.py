40e6e64d3f1d385f32c550da47b29d86dda82bc0
Michael Ketchel <michael.ketchel@gmail.com>
2023-12-06 03:53:38 -0500

    Cleanup Template

--- dayX.py
+++ dayX.py
@@ -1,8 +1,3 @@
-# This is a sample Python script.
-import re
-
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 def part1():
     with open("input/dayX") as inputfile:
@@ -15,9 +10,7 @@ def part2():
             pass
 
 
-
-# Press the green button in the gutter to run the script.
 if __name__ == '__main__':
     part1()
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
