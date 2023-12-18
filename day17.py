9adf6ffdfa6fc326fe9c11d68d47e0e1e7e54282
Michael Ketchel <michael.ketchel@gmail.com>
2023-12-18 05:51:13 -0500

    day17 part 2

--- day17.py
+++ day17.py
@@ -141,13 +141,48 @@ def part1():
 
 # not 1244 or 1241
 def part2():
-    with open("input/day17.sample.txt") as inputfile:
-        for row in inputfile:
-            pass
+    with open("input/day17.txt") as inputfile:
+    # with open("input/day17.sample2.txt") as inputfile:
+        the_data = [[int(x) for x in y] for y in [x.strip() for x in inputfile]]
+        start = (0, 0)
+        end = (len(the_data) - 1, len(the_data[0]) - 1)
+        seen = set()
+        pq = [(0, *start, 0, 0, 0, [(*start, 0, 0)])]
+
+        while pq:
+            heat_loss, row, col, dir_row, dir_col, steps, path = heapq.heappop(pq)
+
+            if (row, col) == end and 3 < steps <= 10:
+                print(heat_loss)
+                # print(path)
+                print(f"Sum of displayed path: {sum_path(the_data, [(state[1], state[0]) for state in path][1:])}")
+                print_in_arrows(the_data, path)
+
+                break
+
+            # If we've seen this state before, discard
+            if (row, col, dir_row, dir_col, steps) in seen:
+                continue
+            seen.add((row, col, dir_row, dir_col, steps))
+            # print((row, col, dir_row, dir_col, steps))
+            # if we have steps left, and aren't idle
+            if steps < 10 and (dir_row, dir_col) != (0, 0):
+                next_row, next_column = (row + dir_row, col + dir_col)
+                if 0 <= next_row < len(the_data) and 0 <= next_column < len(the_data[0]):
+                    new_heat_loss = heat_loss + the_data[next_row][next_column]
+                    heapq.heappush(pq, (new_heat_loss, next_row, next_column, dir_row, dir_col, steps + 1,
+                                        [*path, (next_row, next_column, dir_row, dir_col)]))
+
+            if steps > 3 or (dir_row, dir_col) == (0, 0):
+                for nr, nc, dr, dc in get_neighbors(row, col, the_data):
+                    if (nr, nc) not in [(row + dir_row, col + dir_col), (row - dir_row, col - dir_col)]:
+                        new_heat_loss = heat_loss + the_data[nr][nc]
+                        heapq.heappush(pq, (new_heat_loss
+                                            , nr, nc, dr, dc, 1, [*path, (nr, nc, dr, dc)]))
 
 
 if __name__ == '__main__':
-    part1()
+    part2()
 
 # at most, 3 blocks in a straight line before must turn
 # can't reverse
