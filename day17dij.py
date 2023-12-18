469340dbc9c6b7f0953aa8cc795131ae023e7618
Michael Ketchel <michael.ketchel@gmail.com>
2023-12-18 05:41:52 -0500

    all prettied up

--- day17dij.py
+++ day17dij.py
@@ -49,7 +49,7 @@ def sum_path(matrix, path):
     total = 0
     for step in path:
         x, y = step
-        print(matrix[y][x], end='')
+        # print(matrix[y][x], end='')
         total += matrix[y][x]
     print()
     return total
@@ -67,27 +67,49 @@ def print_in_technicolor(data, path, node_of_interest=(-1,-1)):
         print()
     print()
 
+def print_in_arrows(data, path, node_of_interest=(-1,-1)):
+    # in (row, column)
+    arrow_map = {
+        (0,0): 'X',
+        (1,0) :'v',
+        (-1,0):'^',
+        (0,1):'>',
+        (0,-1):'<',
+    }
+    clean_path = [a[:2] for a in path]
+    for row_index, row in enumerate(data):
+        for col_index, value in enumerate(row):
+            if (row_index, col_index) == node_of_interest[:2]:
+                print(Back.RED + Fore.BLACK + str(value) + Style.RESET_ALL + " ", end='')
+                continue
+            if (row_index, col_index) in clean_path:
+                index = clean_path.index((row_index, col_index))
+                path_entry = path[index]
+                print(Back.GREEN + Fore.BLACK + arrow_map[tuple(path_entry[2:4])] + Style.RESET_ALL + " ", end='')
+                continue
+            print(str(value) + " ", end='')
+        print()
+    print()
 
 def part1():
     with open("input/day17.txt") as inputfile:
+    # with open("input/day17.sample.txt") as inputfile:
         the_data = [[int(x) for x in y] for y in [x.strip() for x in inputfile]]
         start = (0,0)
         end = (len(the_data)-1, len(the_data[0])-1)
         seen = set()
-        pq = [(0,*start,0,0,0)]
-        prev = {start: None}
-        distances = {start: 0}
+        pq = [(0, *start, 0, 0, 0, [(*start, 0, 0)])]
 
         while pq:
-            heat_loss, row, col, dir_row, dir_col, steps = heapq.heappop(pq)
+            heat_loss, row, col, dir_row, dir_col, steps, path = heapq.heappop(pq)
 
             if (row, col) == end:
                 print(heat_loss)
-                break
+                # print(path)
+                print(f"Sum of displayed path: {sum_path(the_data,[(state[1],state[0]) for state in path][1:])}" )
+                print_in_arrows(the_data, path)
 
-            # Discard invalid states
-            if row < 0 or row >= len(the_data) or col < 0 or col >= len(the_data[0]):
-                continue
+                break
 
             # If we've seen this state before, discard
             if (row, col, dir_row, dir_col, steps) in seen:
@@ -99,28 +121,14 @@ def part1():
                 next_row, next_column = (row + dir_row, col + dir_col)
                 if 0 <= next_row < len(the_data) and 0 <= next_column < len(the_data[0]):
                     new_heat_loss = heat_loss + the_data[next_row][next_column]
-                    heapq.heappush(pq, (new_heat_loss, next_row, next_column, dir_row, dir_col, steps+1))
-                    # if new_heat_loss < distances.get((next_row, next_column), float('infinity')):
-                    #     distances[(next_row, next_column)] = new_heat_loss
-                    #     prev[(next_row, next_column)] = (row, col)
+                    heapq.heappush(pq, (new_heat_loss, next_row, next_column, dir_row, dir_col, steps+1, [*path, (next_row, next_column, dir_row, dir_col)]))
 
 
             for nr, nc, dr, dc in get_neighbors(row, col, the_data):
                 if (nr, nc) not in [(row+dir_row, col+dir_col), (row-dir_row, col-dir_col)]:
                     new_heat_loss = heat_loss + the_data[nr][nc]
                     heapq.heappush(pq, (new_heat_loss
-                        , nr, nc, dr, dc, 1))
-                    # if new_heat_loss < distances.get((nr, nc), float('infinity')):
-                    #     distances[(nr, nc)] = new_heat_loss
-                    #     prev[(nr, nc)] = (row, col)
-
-        # print(f"{distances[end]}")
-        # # path = [(x,y) for y,x in reconstruct_path(prev, end)]
-        # path = reconstruct_path(prev, end)
-        # print("The reconstruction just ain't quite right yet.")
-        # # Gotta flip it for the render cuz we ain't rewriting that
-        # print_in_technicolor(the_data, [(x,y) for y,x in path])
-        # print(sum_path(the_data, path))
+                        , nr, nc, dr, dc, 1, [*path, (nr, nc, dr, dc)]))
 
 # not 1244 or 1241
 def part2():
