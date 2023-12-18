c375079aedec3c80dfb6855240ac01b35c7f7551
Michael Ketchel <michael.ketchel@gmail.com>
2023-12-18 06:20:42 -0500

    Cleanups cuz I can

--- day17.py
+++ day17.py
@@ -137,34 +137,7 @@ def part2():
 
 
 if __name__ == '__main__':
+    print("Part 1:")
+    part1()
+    print("\nPart 2:")
     part2()
-
-# at most, 3 blocks in a straight line before must turn
-# can't reverse
-
-
-#
-#
-# 1  function Dijkstra(Graph, source):
-# 2      dist[source] ← 0                           // Initialization
-# 3
-# 4      create vertex priority queue Q
-# 5
-# 6      for each vertex v in Graph.Vertices:
-# 7          if v ≠ source
-# 8              dist[v] ← INFINITY                 // Unknown distance from source to v
-# 9              prev[v] ← UNDEFINED                // Predecessor of v
-# 10
-# 11         Q.add_with_priority(v, dist[v])
-# 12
-# 13
-# 14     while Q is not empty:                      // The main loop
-# 15         u ← Q.extract_min()                    // Remove and return best vertex
-# 16         for each neighbor v of u:              // Go through all v neighbors of u
-# 17             alt ← dist[u] + Graph.Edges(u, v)
-# 18             if alt < dist[v]:
-# 19                 dist[v] ← alt
-# 20                 prev[v] ← u
-# 21                 Q.decrease_priority(v, alt)
-# 22
-# 23     return dist, prev
