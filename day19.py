6eca44e4c7a0cc844fd41efd25b39046486435b1
Michael Ketchel <michael.ketchel@gmail.com>
2023-12-19 01:18:06 -0500

    start part19

---
+++ day19.py
@@ -0,0 +1,16 @@
+
+def part1():
+    with open("input/dayX") as inputfile:
+        for row in inputfile:
+            pass
+
+def part2():
+    with open("input/dayX") as inputfile:
+        for row in inputfile:
+            pass
+
+
+if __name__ == '__main__':
+    part1()
+
+
