ff3b8f9ca396a8d39a38a48147d7d20e90d44699
Michael Ketchel <michael.ketchel@gmail.com>
2023-12-19 14:12:53 -0500

    more fumbling

--- day19.py
+++ day19.py
@@ -1,6 +1,7 @@
 
-import re
+import re, math
 from pprint import pp
+import functools
 
 class Workflow:
     def __init__(self, name, final_rule, rules=None):
@@ -43,10 +44,14 @@ class Rule:
         return self.target if eval(f"{part[self.variable]} {self.operator} {self.operand}") else False
 
     def __str__(self):
-        return f"{self.variable} {self.operator} {self.operand}"
+        return f"{self.variable} {self.operator} {self.operand} -> {self.target}"
+
+    def __repr__(self):
+        return self.__str__()
 
 def part1():
-    with open("input/day19.txt") as inputfile:
+    with open("input/day19.tim.txt") as inputfile:
+    # with open("input/day19.txt") as inputfile:
         workflows = {}
         main_re = r"(?P<name>\w+){(?P<rules>(?:\w+\W\d+:\w+,)+)(?P<final_rule>\w+)}"
         rule_re = r"(?P<variable>\w+)(?P<operator>\W)(?P<operand>\d+):(?P<target>\w+),"
@@ -81,15 +86,86 @@ def part1():
             if workflows['in'].fancy_evaluate(workflows, part) == 'A':
                 total_part_sum += sum([int(v) for v in part.values()])
         print(total_part_sum)
+    return workflows, parts
+
+def explore(workflows, workflow_name='in', rules=None):
+    rules = rules if rules else []
+    if workflow_name == 'R':
+        return False
+    elif workflow_name == 'A':
+        # print(rules)
+        return [rules]
+    else:
+        # return [explore(workflows, rule.target, [*rules, (workflow_name, rule)]) for rule in workflows[workflow_name].rules]
+        ret_rules = []
+        for rule in workflows[workflow_name].rules:
+            res = explore(workflows, rule.target, [*rules, (workflow_name, rule)])
+            if res:
+                ret_rules.extend(res)
+        return ret_rules
+
 
 
 def part2():
-    with open("input/dayX") as inputfile:
-        for row in inputfile:
-            pass
+    workflows, parts = part1()
+    print("\nPart 2:")
 
 
+    chain_limits = []
+    chains = explore(workflows)
+    # print(chains)
+    for chain in chains:
+        non_inclusive_limits = {
+            'x': [0, 4001],
+            'm': [0, 4001],
+            'a': [0, 4001],
+            's': [0, 4001],
+        }
+        for workflow_name, rule in chain:
+            operand = int(rule.operand)
+            if rule.operator == '<' and non_inclusive_limits[rule.variable][1] > operand:
+                non_inclusive_limits[rule.variable][1] = operand
+            elif rule.operator == '>' and non_inclusive_limits[rule.variable][0] < operand:
+                non_inclusive_limits[rule.variable][0] = operand
+        chain_limits.append({k:[v[0]+1, v[1]-1] for k,v in non_inclusive_limits.items()})
+        # pp(non_inclusive_limits)
+        # sizes = [mx-mn-2 for mn, mx in non_inclusive_limits.values()]
+        # pp(sizes)
+        # print(functools.reduce(lambda a, b: a*b, sizes))
+    # pp(chain_limits)
+
+    print("[")
+    for entry in zip(chains, chain_limits):
+        print(f" (\n   {entry[0]},\n   {entry[1]}\n ),")
+    print("]")
+        # print(f"{entry[1]} <== {entry[0]}")
+
+
+    # def multinomial_coefficient_single(data):
+    #     total_values = sum([max(v) - min(v) + 1 for v in data.values()])
+    #     return [math.comb(total_values, len(v) - (min(v) - 1)) for v in data.values()]
+    #
+    # def multinomial_coefficient_multiple(data):
+    #     coefficients = [multinomial_coefficient_single(d) for d in data]
+    #     return math.prod([math.comb(sum(c), c) for c in coefficients])
+    #
+    # unique_combinations = multinomial_coefficient_multiple(chain_limits)
+    # print(unique_combinations)
+    #
+    # overall_non_inclusive_limits = {
+    #     'x': [functools.reduce(min, [v['x'][0] for v in chain_limits]), functools.reduce(max, [v['x'][1] for v in chain_limits])],
+    #     'm': [functools.reduce(min, [v['m'][0] for v in chain_limits]), functools.reduce(max, [v['m'][1] for v in chain_limits])],
+    #     'a': [functools.reduce(min, [v['a'][0] for v in chain_limits]), functools.reduce(max, [v['a'][1] for v in chain_limits])],
+    #     's': [functools.reduce(min, [v['s'][0] for v in chain_limits]), functools.reduce(max, [v['s'][1] for v in chain_limits])],
+    # }
+    # # print(overall_non_inclusive_limits)
+    # sizes = [mx-mn-2 for mn, mx in overall_non_inclusive_limits.values()]
+    # # pp(sizes)
+    # print(functools.reduce(lambda a, b: a*b, sizes))
+
+    # sample val: 167409079868000
+    # compare to: 86271264186651
 if __name__ == '__main__':
-    part1()
+    part2()
 
 
